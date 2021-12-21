e2f6fce3457df2bddc2ce5e0511b8492c1d379fa
Chen XM <oscar999@mail.ustc.edu.cn>
2021-12-21 23:49:35 +0800

    fix

--- src/main/java/cn/osxm/ssmi/chp02/ApplicationContextDemo.java
+++ src/main/java/cn/osxm/ssmi/chp02/ApplicationContextDemo.java
@@ -1,7 +1,7 @@
 /**
  * @Title: ApplicationContextDemo.java
  * @Package cn.osxm.ssmi.chp2
- * @Description: TODO
+ * @Description: 这里演示Spring加载配置文件的方式
  * @author osxm:oscarxueming
  * @date 2018年12月4日 上午6:50:20
  * @version V1.0
@@ -21,7 +21,7 @@ public class ApplicationContextDemo {
 
 	/**
 	 * @Title: main
-	 * @Description: TODO
+	 * @Description: 方式 1 -5, 选择一种使用
 	 * @param args
 	 */
 
@@ -38,11 +38,14 @@ public class ApplicationContextDemo {
 		// ClassPathXmlApplicationContext("cn/osxm/ssmi/chp2/applicationContext.xml");
 		//context = new FileSystemXmlApplicationContext("classpath:cn/osxm/ssmi/chp2/applicationContext.xml ");
 
+		//3. 配置文件位于项目的根目录
 		//context = new FileSystemXmlApplicationContext("applicationContext.xml");
 		
+		//4. 配置文件在项目根路径的子目录下
 		//context = new FileSystemXmlApplicationContext("config/applicationContext.xml");
 		//context = new FileSystemXmlApplicationContext("file:///D:/devworkspace/ecpphoton/ssmi/applicationContext.xml");
 		
+		//5. 动态加载
 		GenericXmlApplicationContext context = new GenericXmlApplicationContext();
 		context.setValidating(false);
 		context.load("classpath:applicationContext.xml");
